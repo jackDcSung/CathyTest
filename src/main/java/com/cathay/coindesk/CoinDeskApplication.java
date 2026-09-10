7773676b6d2d448f1b1efdd65efa8b1fdcc7cc51
jackDcSung <174756554+jackDcSung@users.noreply.github.com>
2026-09-10 22:36:04 +0800

    建立 Maven + Spring Boot 專案

---
+++ src/main/java/com/cathay/coindesk/CoinDeskApplication.java
@@ -0,0 +1,12 @@
+package com.cathay.coindesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinDeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinDeskApplication.class, args);
+    }
+}
